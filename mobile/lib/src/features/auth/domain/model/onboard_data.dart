d8ee8324b928572de18f5a5af6d0b82bc73182f4
Eduardo374 <dudualvim20@gmail.com>
2022-04-08 18:33:05 -0300

    Criação do Onboarding.

---
+++ mobile/lib/src/features/auth/domain/model/onboard_data.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class Onboarding {
+  final String image;
+
+  Onboarding({
+    required this.image,
+  });
+}
+
+List<Onboarding> onboardingContents = [
+  Onboarding(
+    image: 'lib/assets/images/onboarding1.png',
+  ),
+  Onboarding(
+    image: 'lib/assets/images/onboarding1.png',
+  ),
+  Onboarding(
+    image: 'lib/assets/images/onboarding2.png',
+  ),
+];
